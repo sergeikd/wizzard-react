f6ae12ce0b1334affc8dc09c20628b9ede143061
skudryavtsev <s2.kudryavtsev@itransition.com>
2018-12-18 15:59:22 +0300

    try to publish

---
+++ precache-manifest.35817b05f72bcd93e4be5bfb80ef71dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a65397143d6d924f05c",
+    "url": "/wizzard-react/static/js/runtime~main.9a653971.js"
+  },
+  {
+    "revision": "43ab39dfeef3b5c9015c",
+    "url": "/wizzard-react/static/js/main.43ab39df.chunk.js"
+  },
+  {
+    "revision": "f5be82589ff580063075",
+    "url": "/wizzard-react/static/js/1.f5be8258.chunk.js"
+  },
+  {
+    "revision": "43ab39dfeef3b5c9015c",
+    "url": "/wizzard-react/static/css/main.79d07e76.chunk.css"
+  },
+  {
+    "revision": "7ea1d0f747390feafc9578d8f3830fc2",
+    "url": "/wizzard-react/index.html"
+  }
+];
