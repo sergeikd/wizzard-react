45210d56b57259c1e00cf9cd0b05ac1bc7ab4bd0
skudryavtsev <s2.kudryavtsev@itransition.com>
2018-12-18 20:15:05 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ class App extends Component {
     this.state = {
       pageId: 0,
       content: [],
+      summary: [],
     };
     this.cars = { ...cars };
     this.pages = pages;
-    this.summary = [];
+    this.content = [];
   }
 
   componentDidMount() {
@@ -38,12 +39,21 @@ class App extends Component {
     }));
   }
 
-  
+  onItemClick = (e) => {
+    const id = e.target.value;
+    if (this.state.pageId < this.pages.length - 1) {
+      let tempSummary = this.state.summary;
+      tempSummary[this.state.pageId] = id;
+      tempSummary.length = this.state.pageId + 1;
+      this.setState(prevState => ({summary: prevState.summary}))
+    }
+  }
+
   getContent = (pageId) => {
     let entity = [];
     switch (pageId) {
       case 1:
-        entity = this.cars.model.filter(x => x.brandId === this.summary[0]);
+        entity = this.cars.model.filter(x => x.brandId === this.state.summary[0]);
         break;
       case 2:
         entity = this.cars.engine.filter(x => this.cars.model[x.id].availableEngineIds.includes(x.id))
@@ -52,23 +62,14 @@ class App extends Component {
         entity = this.cars.gear.filter(x => this.cars.model[x.id].availableGearsIds.includes(x.id));
         break;
       case 4:
-        for (let i = 0; i < this.summary.length; i++) {
-          entity[i] = { id: i, name: `${pages[i].entity}: ${cars[pages[i].entity][this.summary[i] - 1].name}` };
+        for (let i = 0; i < this.state.summary.length; i++) {
+          entity[i] = { id: i, name: `${pages[i].entity}: ${cars[pages[i].entity][this.state.summary[i] - 1].name}` };
         }
         break;
       default:
         entity = this.cars.brand;
     }
     return entity;
-  }
-
-  onItemClick = (e) => {
-    const index = e.target.value;
-    if (this.state.pageId < this.pages.length - 1) {
-      this.summary[this.state.pageId] = index;
-      this.summary.length = this.state.pageId + 1;
-      this.forceUpdate();
-    }
   }
 
   render() {
@@ -78,13 +79,13 @@ class App extends Component {
           <NavButton
             pageId={this.state.pageId}
             maxPages={this.pages.length}
-            summary={this.summary}
+            summary={this.state.summary}
             onClickTo={this.onClickBack}
             name='Back' />
           <NavButton
             pageId={this.state.pageId}
             maxPages={this.pages.length}
-            summary={this.summary}
+            summary={this.state.summary}
             onClickTo={this.onClickFwd}
             name='Forward' />
         </div>
@@ -92,7 +93,7 @@ class App extends Component {
           content={this.state.content}
           pages={this.pages}
           pageId={this.state.pageId}
-          summary={this.summary}
+          summary={this.state.summary}
           onItemClick={this.onItemClick} />
       </>
     );
